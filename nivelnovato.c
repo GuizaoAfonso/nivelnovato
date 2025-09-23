eb29b97df20b4cb6ac145966975617c66c2246d6
GuizaoAfonso <guilhermeafonso99@hotmail.com>
2025-09-23 17:09:53 +0000

    atualizacao

--- nivelnovato.c
+++ nivelnovato.c
@@ -2,188 +2,62 @@
 #include <stdlib.h>
 #include <string.h>
 
-//Constantes globais
+// Constantes globais
 #define max_territorio 5
-#define tam_string 100
 
-//Definição da Estrutura
-struct Territorio
-{
+// Definição da Estrutura
+struct Territorio {
     char nome[30];
     char cor[10];
     int tropas;
 };
 
-//Função para limpar o buffer de entrada
-void limparBufferEntrada(){
+// Função para limpar o buffer de entrada
+void limparBufferEntrada() {
     int c;
-    while ((c = getchar()) != '\n' && c != EOF);    
+    while ((c = getchar()) != '\n' && c != EOF);
 }
-
 
 // Função principal
 int main() {
     struct Territorio cadastro[max_territorio];
-    int totalTerritorio = 0;
-    int opcao;
 
-
-//Laço principal do menu
-do {
-    
-    //Exibe o menu de opções
     printf("====================================\n");
-    printf("      CADASTRANDO TERRITÓRIOS\n");
-    printf("====================================\n");
-    printf("1- Cadastrar novo Território\n");
-    printf("2- Listar todos os Territórios\n");
-    printf("0- Sair\n");
-    printf("====================================\n");
-    printf("Escolha uma opção:\n");
+    printf("   CADASTRO DE %d TERRITÓRIOS\n", max_territorio);
+    printf("====================================\n\n");
 
+    // Cadastro dos 5 territórios em sequência
+    for (int i = 0; i < max_territorio; i++) {
+        printf("--- Cadastro do Território %d ---\n", i + 1);
 
-    //Lê a opção do usuário
-    scanf("%d", &opcao);
-    limparBufferEntrada(); //limpa o \n deixado pelo scanf
+        printf("Digite o nome do Território: ");
+        fgets(cadastro[i].nome, sizeof(cadastro[i].nome), stdin);
+        cadastro[i].nome[strcspn(cadastro[i].nome, "\n")] = '\0';
 
-    //Processamento da opção
-    switch (opcao)
-    {
-    case 1: //Cadastro de Territórios
-        printf("--- Cadastro do Território 1 ---\n\n");
+        printf("Digite a cor do Exército: ");
+        fgets(cadastro[i].cor, sizeof(cadastro[i].cor), stdin);
+        cadastro[i].cor[strcspn(cadastro[i].cor, "\n")] = '\0';
 
-        if (totalTerritorio < max_territorio)
-        {
-            printf("Digite o nome do Território:");
-            fgets(cadastro[totalTerritorio].nome, tam_string, stdin);
+        printf("Digite o número de Tropas: ");
+        scanf("%d", &cadastro[i].tropas);
+        limparBufferEntrada();
 
-            printf("Digite a cor do Exército:");
-            fgets(cadastro[totalTerritorio].cor, tam_string, stdin);
+        printf("\n");
+    }
 
-            cadastro[totalTerritorio].nome[strcspn(cadastro[totalTerritorio].nome, "\n")] = '\0';
-            cadastro[totalTerritorio].cor[strcspn(cadastro[totalTerritorio].cor, "\n")] = '\0';
-                        
-            printf("Digite o número de Tropas:");
-            scanf("%d", &cadastro[totalTerritorio].tropas);
-            limparBufferEntrada();
+    // Exibe o "mapa" dos territórios cadastrados
+    printf("\n====================================\n");
+    printf("       MAPA DE TERRITÓRIOS\n");
+    printf("====================================\n\n");
 
-            totalTerritorio++;
+    for (int i = 0; i < max_territorio; i++) {
+        printf("Território %d: %s\n", i + 1, cadastro[i].nome);
+        printf("Cor do Exército: %s\n", cadastro[i].cor);
+        printf("Tropas: %d\n", cadastro[i].tropas);
+        printf("-----------------------------\n");
+    }
 
-        printf("--- Cadastro do Território 2 ---\n\n");
+    printf("\nFim do cadastro e exibição do mapa!\n");
 
-            printf("Digite o nome do Território:");
-            fgets(cadastro[totalTerritorio].nome, tam_string, stdin);
-
-            printf("Digite a cor do Exército:");
-            fgets(cadastro[totalTerritorio].cor, tam_string, stdin);
-
-            cadastro[totalTerritorio].nome[strcspn(cadastro[totalTerritorio].nome, "\n")] = '\0';
-            cadastro[totalTerritorio].cor[strcspn(cadastro[totalTerritorio].cor, "\n")] = '\0';
-                        
-            printf("Digite o número de Tropas:");
-            scanf("%d", &cadastro[totalTerritorio].tropas);
-            limparBufferEntrada();
-
-            totalTerritorio++;
-
-        printf("--- Cadastro do Território 3 ---\n\n");
-
-            printf("Digite o nome do Território:");
-            fgets(cadastro[totalTerritorio].nome, tam_string, stdin);
-
-            printf("Digite a cor do Exército:");
-            fgets(cadastro[totalTerritorio].cor, tam_string, stdin);
-
-            cadastro[totalTerritorio].nome[strcspn(cadastro[totalTerritorio].nome, "\n")] = '\0';
-            cadastro[totalTerritorio].cor[strcspn(cadastro[totalTerritorio].cor, "\n")] = '\0';
-                        
-            printf("Digite o número de Tropas:");
-            scanf("%d", &cadastro[totalTerritorio].tropas);
-            limparBufferEntrada();
-
-            totalTerritorio++;
-
-        printf("--- Cadastro do Território 4 ---\n\n");
-
-            printf("Digite o nome do Território:");
-            fgets(cadastro[totalTerritorio].nome, tam_string, stdin);
-
-            printf("Digite a cor do Exército:");
-            fgets(cadastro[totalTerritorio].cor, tam_string, stdin);
-
-            cadastro[totalTerritorio].nome[strcspn(cadastro[totalTerritorio].nome, "\n")] = '\0';
-            cadastro[totalTerritorio].cor[strcspn(cadastro[totalTerritorio].cor, "\n")] = '\0';
-                        
-            printf("Digite o número de Tropas:");
-            scanf("%d", &cadastro[totalTerritorio].tropas);
-            limparBufferEntrada();
-
-            totalTerritorio++;
-
-        printf("--- Cadastro do Território 5 ---\n\n");
-
-            printf("Digite o nome do Território:");
-            fgets(cadastro[totalTerritorio].nome, tam_string, stdin);
-
-            printf("Digite a cor do Exército:");
-            fgets(cadastro[totalTerritorio].cor, tam_string, stdin);
-
-            cadastro[totalTerritorio].nome[strcspn(cadastro[totalTerritorio].nome, "\n")] = '\0';
-            cadastro[totalTerritorio].cor[strcspn(cadastro[totalTerritorio].cor, "\n")] = '\0';
-                        
-            printf("Digite o número de Tropas:");
-            scanf("%d", &cadastro[totalTerritorio].tropas);
-            limparBufferEntrada();
-
-            totalTerritorio++;
-
-            printf("Territórios Cadastrados com Sucesso!\n");
-        } else {
-            printf("Territórios cheios! Não é possível cadastrar mais Territórios.\n");
-        }
-        
-        printf("\nPressione Enter para continuar...\n");
-        getchar(); // Pausa para o usuário ler a mensagem
-        break;
-
-    case 2: //Listagem de Territórios
-        printf("---Listas de Territórios cadastrados---\n\n");
-
-        if (totalTerritorio == 0)
-        {
-            printf("Nenhum Território cadastrado ainda.\n");
-        } else
-        {
-            for (int i = 0; i < totalTerritorio; i++)
-            {
-                printf("-----------------------------\n");
-                printf("Território: %d\n", i + 1);
-                printf("Cor do Exército: %d\n", cadastro[i].cor);
-                printf("Tropa: %s\n", cadastro[i].tropa);
-                printf("-----------------------------\n");
-            
-        }
-
-        //pausa para leitura
-
-        printf("\nPressione Enter para continuar...\n");
-        getchar();
-        break;
-
-    case 0: //sair
-        printf("\nSaindo do Sistema...\n");
-        break;
-
-    default: //opção inválida
-        printf("\nOpção inválida, tente novamente...\n");
-        printf("\nPressione Enter para continuar...\n");
-        getchar();
-        break;
-}
-        
-
-} while (opcao != 0);
-
-return 0;  //fim do programa
-
+    return 0;
 }
